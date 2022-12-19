66d8d51279b3aad3da3cc6bc9e368bc9091c96c4
Hrvoje <hbrkljacic@gmail.com>
2022-12-19 10:12:26 -0500

    SpaceCraftTesting

---
+++ src/main/java/com/cydeo/spacecraft/SpaceCraftTestingHApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spacecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceCraftTestingHApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceCraftTestingHApplication.class, args);
+    }
+
+}
